b4e5ba00c290203f8a8eaaf9ee0f2d367bd10fc7
Szeto <Lunerfox@gmail.com>
2015-05-31 21:01:36 +0800

    Added an Objective C to Swift Bridge to integrate cocoa pods into the project.
    Ref: http://michal.codes/integrating-cocoapods-with-a-swift-project/

---
+++ haikubo/ObjC-Bridge.h
@@ -0,0 +1,17 @@
+//
+//  ObjC-Bridge.h
+//  haikubo
+//
+//  Created by Leo Szeto on 5/31/15.
+//  Copyright (c) 2015 Wrightplay. All rights reserved.
+//
+//  Used to bridge Objective C libraries together.
+//  Ref: http://michal.codes/integrating-cocoapods-with-a-swift-project/
+
+
+#ifndef haikubo_ObjC_Bridge_h
+#define haikubo_ObjC_Bridge_h
+
+#import <Onboard/OnboardingViewController.h>
+
+#endif
